dccb8e80cb4c7649a710165dbec88471cf6407ac
Сириус <@->
2021-10-11 00:17:56 +0300

    Добавьте файлы проекта.

---
+++ Zadanie_7_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные первого треугольника:");
+            double s1 = CalcTriangleSquare();
+            Console.WriteLine("Введите данные второго треугольника:");
+            double s2 = CalcTriangleSquare();
+            Console.WriteLine();
+            if (s1>s2)
+            {
+                Console.WriteLine("Площадь первого треугольника {0:f2} больше площади второго треугольника {1:f2}.",s1,s2);
+            }
+            else if (s1==s2)
+            {
+                Console.WriteLine("Площади треугольников равны {0}.",s1);
+            }
+            else
+            {
+                Console.WriteLine("Площадь второго треугольника {0:f2} больше площади первого треугольника {1:f2}.", s2, s1);
+            }
+            Console.ReadKey();
+        }
+
+        static double CalcTriangleSquare ()
+        {
+            Console.Write("Длина стороны а:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Длина стороны b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Длина стороны c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            double P = a + b + c;
+            double p2 = P / 2;
+            double S=Math.Sqrt(p2*(p2-a)*(p2-b)*(p2-c));
+            return S;
+        }
+    }
+}
---
+++ Zadanie_7_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double V, S;
+            CalcCube(a,out V, out S);
+            Console.WriteLine();
+            Console.WriteLine("Объем куба равен {0}, а площадь поверхности куба равна: {1}",V,S);
+            Console.ReadKey();
+        }
+        static void CalcCube (int a, out double V, out double S)
+        {
+            V = Math.Pow(a,3);
+            S = 6 * a * a;
+        }
+    }
+}
